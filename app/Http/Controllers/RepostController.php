90eb8ab139c96d1a12961580dfe227543a21a1e5
FraserProvan2 <FraserProvan2@gmail.com>
2019-12-23 21:39:24 +0000

    refactor to reddit API class

--- app/Http/Controllers/RepostController.php
+++ app/Http/Controllers/RepostController.php
@@ -3,31 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use RedditAPI;
 
 class RepostController extends Controller
 {
+    public $attempts = 0;
+
     public function run()
     {
+        $this->attempts++; // keep track of progress
+        Log::debug('process start attempt #' . $this->attempts);
+
         // create Reddit API client
         $reddit_api = new RedditAPI;
 
         // find a sub reddit
         $sub_reddits = $reddit_api->get('/subreddits/mine/subscriber');
-        $selected_subreddit = $sub_reddits->data->children[rand(0, 4)]->data->display_name;
-        dump('/r/' . $selected_subreddit);
+        $selected_subreddit = $sub_reddits->data->children[rand(0, count($sub_reddits->data->children) - 1)]->data->display_name;
+        Log::debug('chosen subreddit: /r/' . $selected_subreddit);
 
         // find a post
         $endpoint = '/r/' . $selected_subreddit . '/search';
         $query_string = http_build_query([
             'q' => $selected_subreddit, // query
             't' => 'year', // time
+            'limit' => 100
         ]);
         $request_url = $endpoint . '?' . $query_string;
         $results = $reddit_api->get($request_url);
 
         // look for link flair
         $selected_post = null;
+        shuffle($results->data->children);
         foreach($results->data->children as $post) {
             if (isset($post->data->post_hint) && 
                 $post->data->post_hint === "link" && 
@@ -38,27 +46,28 @@ class RepostController extends Controller
             }
         };  
         if (!isset($selected_post)) {
-            dump('failed to find link...');
+            Log::warning('failed to find link...');
             $this->run(); // rerun if no link found
         }
-        dump('https://reddit.com' . $selected_post->permalink);
+        Log::debug('chosen post: https://reddit.com' . $selected_post->permalink);
 
         // repost 
         $cloned_post = [
             'title' => $selected_post->title,
-            'sr' => 'test', //$selected_post->subreddit,
+            'sr' => $selected_post->subreddit,
             'url' => $selected_post->url,
             'kind' => 'link',
             // 'uh' => 'f0f0f0f0', 
         ];
-        dump($cloned_post);
-        $result = $reddit_api->post($cloned_post);
+        Log::debug('cloned data: ', $cloned_post);
+        $result = $reddit_api->createPost($cloned_post);
         
         if (!$result->success) {
-            dump('failed to post... Here we go again');
-            // $this->run(); // failure is NOT an option
+            Log::warning('failed to post... Here we go again');
+            $this->run(); // failure is NOT an option
         }
 
-        dd($result->success);
+        Log::debug('post success');
+        return response('success', 200);
     }
 }
--- app/RedditAPI.php
+++ app/RedditAPI.php
@@ -1,74 +1,117 @@
 <?php
 
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class RedditAPI
 {
-	public $access_token;
+	/*--------------------------------------------------------------------------
+	| Properties
+	|--------------------------------------------------------------------------*/
 
+	public $access_token;
+	private $expires;
+
+	/*--------------------------------------------------------------------------
+	| Authentication
+	|--------------------------------------------------------------------------*/
+
+	/**
+	 * Construct, handle Auth per request, use cached access token
+	 * if avaliable, keep track if expired 
+	 */
 	public function __construct()
 	{
-		session()->flush();
-
 		// handle Oauth2
-		$this->access_token = session()->get('access_token');
-		if (!$this->access_token) {
-			$this->getAccessToken();
-		}
 		$this->access_token = session()->get('access_token')->access_token;
+		$this->expires = session()->get('expires');
+
+		// create new token
+		if (!isset($this->access_token) || now() < $this->expires) {
+			$this->access_token = $this->getAccessToken()->access_token;
+			$this->expires = Carbon::now()->addSeconds($this->access_token->expires_in);
+
+			session()->put('access_token', $this->access_token);
+			session()->put('expires', $this->expires);
+
+			Log::debug('new access token created');
+		} 
+		// use valid cached access token
+		else {
+			Log::debug('using cached token');
+		}
 	}
 
-	public function getAccessToken()
+	/**
+	 * Gets a new access token
+	 * 
+	 * @return string access_token Instace
+	 */
+	private function getAccessToken()
 	{
-		$username = config('reddit_api.username');
-		$password = config('reddit_api.password');
-		$client_id = config('reddit_api.client_id');
-		$secret = config('reddit_api.secret');
-
-		$post_data = array(
+		return $this->curl('https://www.reddit.com/api/v1/access_token', [
 			'grant_type' => 'password',
-			'username' => $username,
-			'password' => $password
-		);
-
-		$ch = curl_init('https://www.reddit.com/api/v1/access_token');
-		curl_setopt($ch, CURLOPT_USERPWD, $client_id . ':' . $secret);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-
-		$response_raw = curl_exec($ch);
-		$response = json_decode($response_raw);
-
-		curl_close($ch);
-
-		session()->put('access_token', $response);
+			'username' => config('reddit_api.username'),
+			'password' => config('reddit_api.password')
+		]);
 	}
 
-	public function get($endpoint)
+	/*--------------------------------------------------------------------------
+	| API resource requests
+	|--------------------------------------------------------------------------*/
+
+	/**
+	 * Generic GET request for reddit API
+	 * 
+	 * @param string endpoint
+	 * @return object response
+	 */
+	public function get(string $endpoint)
 	{
-		$ch = curl_init(config('reddit_api.api_url') . $endpoint);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_USERAGENT, config('reddit_api.user_agent') . $endpoint);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: bearer " . $this->access_token));
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-
-		$response_raw = curl_exec($ch);
-		$response = json_decode($response_raw);
-		curl_close($ch);
-
-		return $response;
+		return $this->curl(config('reddit_api.api_url') . $endpoint);
 	}
 
-	public function post($post_data)
+	/**
+	 * Create a new reddit post
+	 * 
+	 * @param array post data
+	 * @return object response
+	 */
+	public function createPost(array $post_data)
 	{
-		$ch = curl_init('https://oauth.reddit.com/api/submit');
+		return $this->curl('https://oauth.reddit.com/api/submit', $post_data);
+	}
+
+	/*--------------------------------------------------------------------------
+	| Utility
+	|--------------------------------------------------------------------------*/
+
+	/**
+	 * Base cURL function
+	 * 
+	 * @param string URL
+	 * @param array post data
+	 * @param boolean user password
+	 * @return object response
+	 */
+	public function curl(string $url, array $post_data = null, boolean $userpwd = null)
+	{
+		Log::debug('curl request: ' . $url . ' ' . json_encode($post_data));
+
+		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_USERAGENT, config('reddit_api.user_agent'));
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: bearer " . $this->access_token));
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+
+		if (isset($post_data)) {
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+		}
+
+		if (isset($userpwd)) {
+			curl_setopt($ch, CURLOPT_USERPWD, config('reddit_api.client_id') . ':' . config('reddit_api.secret'));
+		}
+
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 
 		$response_raw = curl_exec($ch);
