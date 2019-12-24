0a84768d89687dd78a9036b997a1a60b0d82aae1
FraserProvan2 <FraserProvan2@gmail.com>
2019-12-24 02:38:44 +0000

    further frontend + cache busting

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
                 <h4 class="text-center w-100">Session: <span class="text-primary">00:00<span></h4>
                 <div class="small text-center w-100 my-2">Next Post: <span class="text-primary">00:00<span></div>
                 <a href="{{ url('/run') }}" class="btn btn-lg btn-success w-100">Start</a>
-                <div class="small text-secondary text-center w-100 mt-2">4 Posts</div>
+                <div class="small text-secondary text-center w-100 mt-2">0 Post(s)</div>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>sb-23</title>
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
     <script src="https://use.fontawesome.com/c991091b95.js"></script>
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
